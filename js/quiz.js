a7dbe6488b3bb4928c2e40c18c4edbffb771bb2c
Nick R <nmrock@gmail.com>
2017-03-24 14:23:09 -0400

    Adding basic functionality: can select and submit options, javascript checks to see correct answers, sums total number of correct answers

--- js/quiz.js
+++ js/quiz.js
@@ -1,28 +1,71 @@
 (function($, global) {
     
-    function selected() {
-        $(this).addClass('selected');
-    }
+
     
     jsQuiz = function(quizSelector) {
-        self = this;
+        var self = this;
         self.jself = $(quizSelector);
-        self.questions = [];
-        $('.quiz-pane').each(function() {
-            self.questions.push(new quizQuestion(this));
+        self.panes = [];
+        self.jself.find('.quiz-pane').each(function() {
+            self.panes.push(new quizPane(this));
+        });
+            
+        $('*[class=""]').removeAttr('class');
+    }
+
+    jsQuiz.prototype.howManyCorrect = function() {
+        var self = this;
+        var theresults = 0;
+        self.panes.forEach(x => theresults += x.result);
+        console.log(theresults);
+        //return theresults;
+    }
+    quizPane = function(elem) {
+        var self = this;
+        self.result = 0;
+        self.jself = $(elem);
+        self.correct = this.jself.find('.correct-answer').text();
+        self.answers = [];
+        self.jself.find('li').each(function() {
+            self.answers.push(new quizAnswer(this));
         });
         
-        self.selected(this) {
-            
-        }
+        self.button = this.jself.find('button');
+        self.button.click(self.checkAnswer(self));
         
-        $(quizSelector + ' ul li').click(selected);
     }
     
-    quizQuestion = function(q) {
-            this.jself = $(q);
-        //console.log(q);
-            this.correct = this.jself.find('.correct-answer').removeAttr('class').text();
+    quizPane.prototype.checkAnswer = function(self) {
+        return function() {
+            var correct = self.correct;
+            var selected = self.jself.find('.selected').text();
+            if (correct === selected) {
+                console.log("yes, you are correct!");
+                self.result = 1;
+            } else {
+                console.log("WRONG");
+            }
+        }
+        
+    }
+    
+    quizAnswer = function(elem) {
+        var self = this;
+        self.jself = $(elem);
+        if (self.correct = self.jself.hasClass('correct-answer')) {
+            self.jself.removeClass('correct-answer');   
+           
+        }       
+        self.jself.click(self.selected(self.jself));
+    }
+    
+    quizAnswer.prototype.selected = function(closureSelf) {
+            
+            return function() {
+                closureSelf.siblings().removeClass('selected');
+                closureSelf.addClass('selected');
+                
+            }
         
     }
     
@@ -33,6 +76,7 @@
 })(jQuery, window);
 
 myfirstQuiz = new jsQuiz('.myfirstquiz');
+mysecondQuiz = new jsQuiz('.mysecondquiz');
 console.log(myfirstQuiz);
 //console.log(myfirstQuiz);
 
